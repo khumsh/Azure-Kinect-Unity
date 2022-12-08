c684d9ef318cdf9ed314d89c72ccf9148fea555b
DESKTOP-00Q9Q78\Moon <msh0614@khu.ac.kr>
2022-12-09 02:50:47 +0900

    animation loop on off 버튼 추가

--- Assets/Scripts/ModelAvatarAnimation.cs
+++ Assets/Scripts/ModelAvatarAnimation.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class ModelAvatarAnimation : MonoBehaviour
 {
     private Animator anim;
+    private bool loop = true;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        anim.SetBool("Loop", loop);
     }
 
     
     public void PlayAnimation(string animationName)
     {
         anim.SetTrigger(animationName);
+    }
+
+    public void LoopOnOff()
+    {
+        loop = !loop;
+
+        anim.SetBool("Loop", loop);
     }
 }
